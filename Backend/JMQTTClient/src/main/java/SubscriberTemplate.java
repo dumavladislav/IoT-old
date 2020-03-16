afa1666af6c173a343483e491cd3115f1a298d45
Duma <Vladislav.Duma@experian.com>
2020-03-16 19:34:00 +0300

    First Multi threading option

--- Backend/JMQTTClient/src/main/java/SubscriberTemplate.java
+++ Backend/JMQTTClient/src/main/java/SubscriberTemplate.java
@@ -1,5 +1,8 @@
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import static java.lang.Thread.sleep;
 
 public class SubscriberTemplate {
 
@@ -13,10 +16,27 @@ public class SubscriberTemplate {
         // CountDownLatch receivedSignal = new CountDownLatch(10);
         try {
             client.subscribe(topicIn, (topic, msg) -> {
-                byte[] payload = msg.getPayload();
-                // sleep(10000); // - just to test multi threading
-                System.out.println(new String(payload));
-                // receivedSignal.countDown();
+                Thread messageProcessingTh = new Thread(new Runnable() {
+                    private MqttMessage msg;
+                    public Runnable init(MqttMessage msg) {
+                        this.msg = msg;
+                        return this;
+                    }
+
+                    @Override
+                    public void run() {
+                        byte[] payload = this.msg.getPayload();
+                        try {
+                            sleep(10000); // - just to test multi threading
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(new String(payload));
+                        // receivedSignal.countDown();
+                    }
+                }.init(msg));
+                messageProcessingTh.start();
+
             });
             // receivedSignal.await(1, TimeUnit.MINUTES);
         } catch (MqttException e) {
