b450c2a458b258dfb9f3261e877885fdc76b5d20
Duma <Vladislav.Duma@experian.com>
2020-03-31 13:51:36 +0300

    Credentials excluded

--- ESP/MotionSensorLightControl/src/Constants/Credentials.h
+++
@@ -1,8 +0,0 @@
-//#define WIFI_SSID "KhvorikNet-24"
-#define WIFI_SSID "KhvorikMyshkin"
-#define WIFI_PSSWD "Y739ex777"
-//#define MQTT_SERVER "192.168.1.64"
-#define MQTT_SERVER "185.155.19.161"
-#define MQTT_PORT "1883"
-#define MQTT_USER "dumskyhome"
-#define MQTT_PSSWD "S746em99"
