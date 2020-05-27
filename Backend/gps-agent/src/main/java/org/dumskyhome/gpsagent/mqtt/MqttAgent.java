c0e7ad4d6d8f8320668ea39e010ca6034f12a5c9
Duma <Vladislav.Duma@experian.com>
2020-05-27 22:46:20 +0300

    MQTT reconnect

--- Backend/gps-agent/src/main/java/org/dumskyhome/gpsagent/mqtt/MqttAgent.java
+++ Backend/gps-agent/src/main/java/org/dumskyhome/gpsagent/mqtt/MqttAgent.java
@@ -1,9 +1,11 @@
 package org.dumskyhome.gpsagent.mqtt;
 
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dumskyhome.gpsagent.json.JsonGpsDataMessage;
+import org.dumskyhome.gpsagent.persistence.dao.GpsAgentDao;
+import org.dumskyhome.gpsagent.persistence.datamodel.GpsData;
+import org.eclipse.paho.client.mqttv3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,29 +16,50 @@ import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:mqtt.properties")
-public class MqttAgent {
+public class MqttAgent implements MqttCallback {
 
     private static final Logger logger = LoggerFactory.getLogger(MqttAgent.class);
+    private ObjectMapper objectMapper;
 
     private MqttClient mqttClient;
 
     @Value("${mqtt.clientId}")
     private String mqttClientId;
     //private ThreadPoolExecutor executor;
+//    @Autowired
+//    MqttMessageProcessor mqttMessageProcessor;
+
     @Autowired
-    MqttMessageProcessor mqttMessageProcessor;
+    //private GPSAgentService gpsAgentService;
+    private GpsAgentDao gpsAgentDao;
 
     @Autowired
     Environment env;
 
+    MqttAgent() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES);
+    }
+
+    private void init() {
+        try {
+            mqttClient = new MqttClient(env.getProperty("mqtt.serverUrl"), mqttClientId);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
     private boolean connect() {
 
         try {
-            mqttClient = new MqttClient(env.getProperty("mqtt.serverUrl"), mqttClientId);
             MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
             mqttConnectOptions.setUserName(env.getProperty("mqtt.user"));
             mqttConnectOptions.setPassword(env.getProperty("mqtt.password").toCharArray());
             mqttConnectOptions.setConnectionTimeout(Integer.parseInt(env.getProperty("mqtt.connectionTimeout")));
+            mqttConnectOptions.setAutomaticReconnect(true);
             mqttConnectOptions.setCleanSession(true);
 
             mqttClient.connect(mqttConnectOptions);
@@ -49,16 +72,22 @@ public class MqttAgent {
         return false;
     }
 
+    private boolean subscribeToTopics() {
+        try {
+            mqttClient.subscribe(env.getProperty("mqtt.topic.gpsData"));
+            mqttClient.setCallback(this);
+            return true;
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public boolean runMqttService() {
+        init();
         if(connect()) {
             logger.info("Connected to MQTT");
-            try {
-                mqttClient.subscribe(env.getProperty("mqtt.topic.gpsData"));
-                mqttClient.setCallback(mqttMessageProcessor);
-                return true;
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
+            return subscribeToTopics();
         }
         return false;
     }
@@ -74,7 +103,42 @@ public class MqttAgent {
     }
 
 
+    @Override
+    public void connectionLost(Throwable throwable) {
+        logger.error("CONNECTION LOST");
+        while(!connect()) {
+            logger.info("RECONNECTION ATTEMPT");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        logger.error("CONNECTION RESTORED");
+        subscribeToTopics();
+    }
 
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        logger.info("TOPIC:"+ s + " || MESSAGE RECEIVED: " + mqttMessage.toString());
+        if (s.equals(env.getProperty("mqtt.topic.gpsData"))) {
+            logger.info("GPS Data received. Parsing....");
+            JsonGpsDataMessage jsonGpsDataMessage = objectMapper.readValue(mqttMessage.toString(), JsonGpsDataMessage.class);
 
+            logger.info(jsonGpsDataMessage.getData().getLat().toString());
+            //gpsAgentService.saveGpsData(jsonGpsDataMessage).<ResponseEntity>thenApply(ResponseEntity::ok);
+            GpsData gpsData = gpsAgentDao.saveGpsData(new GpsData(
+                    jsonGpsDataMessage.getHeader().getMacAddress(),
+                    jsonGpsDataMessage.getData().getMillis(),
+                    jsonGpsDataMessage.getData().getLat(),
+                    jsonGpsDataMessage.getData().getLng()
+            ));
 
+        }
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
 }
